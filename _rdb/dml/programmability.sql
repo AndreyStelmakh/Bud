81743c749863083d998ecff76e2f8f0d8c56cea8
AndreyStelmakh <andrey.stelmakh@live.ru>
2016-10-02 16:24:18 +0300

    First commit

--- _rdb/dml/programmability.sql
+++ _rdb/dml/programmability.sql
@@ -1,3 +1,38 @@
+﻿
+
+
+
+create function [budget].[RetrieveTitle]
+(
+    @Id uniqueidentifier
+)
+RETURNS NVARCHAR(max)
+AS
+BEGIN
+
+    declare @Title nvarchar(max);
+
+    select @Title = Title
+    from budget.Expenditure
+    where Id = @Id;
+
+    if @Title is not null return @Title;
+
+
+    select @Title = Title
+    from budget.Distributions
+    where Id = @Id;
+
+    return @Title;
+
+END
+
+go
+
+
+
+
+
 
 create procedure budget.Stats
 as
@@ -66,3 +101,4 @@ begin
 
 
 end;
+
